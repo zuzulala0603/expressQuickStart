a27a236435916147ec2a4281320f1dfbdbbd8b7d
zuzulala0603 <zuzulala0603@gmail.com>
2020-07-11 15:08:34 +0900

    app change

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ app.set("views", __dirname + "/views")
 app.set("view engine", "ejs")
 app.engine("html", require("ejs").renderFile)
 app.use("/static", express.static(__dirname + "/public"))
+
 //
-/* const testRouter = require("./routes/test")
-app.use("/", testRouter) */
-//
-//hi
 const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log("server on!")
